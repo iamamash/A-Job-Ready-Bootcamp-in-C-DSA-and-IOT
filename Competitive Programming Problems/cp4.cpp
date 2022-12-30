5edc347cdf33d254dd32b009f700d91bf82a55d2
A M A S H ~ A N S A R I <42666741+iamamash@users.noreply.github.com>
2022-12-31 00:28:55 +0530

    Update cp4.cpp

--- Competitive Programming Problems/cp4.cpp
+++ Competitive Programming Problems/cp4.cpp
@@ -14,39 +14,44 @@
 // Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
 #include <bits/stdc++.h>
 using namespace std;
-double findMedianSortedArrays(int *nums1, int nums1Size, int *nums2, int nums2Size)
+
+class Solution
 {
-    if (nums1Size > nums2Size)
-        return findMedianSortedArrays(nums2, nums2Size, nums1, nums1Size);
-    int low = 0, high = nums1Size, median = ((nums1Size + nums2Size) + 1) / 2;
-    while (low <= high)
+public:
+    double findMedianSortedArrays(vector<int> &nums1, vector<int> &nums2)
     {
-        int mid1 = (low + high) / 2;
-        int mid2 = median - mid1;
+        if (nums1.size() > nums2.size())
+            return findMedianSortedArrays(nums2, nums1);
 
-        int l1 = (mid1 == 0) ? INT_MIN : nums1[mid1 - 1];
-        int l2 = (mid2 == 0) ? INT_MIN : nums2[mid2 - 1];
-        int r1 = (mid1 == nums1Size) ? INT_MAX : nums1[mid1];
-        int r2 = (mid2 == nums2Size) ? INT_MAX : nums2[mid2];
-
-        if (l1 <= r2 && l2 <= r1)
+        int low = 0, high = nums1.size(), median = ((nums1.size() + nums2.size()) + 1) / 2;
+        while (low <= high)
         {
-            if ((nums1Size + nums2Size) % 2 == 0)
-                return (max(l1, l2) + min(r1, r2)) / 2.0;
+            int m1 = (low + high) / 2;
+            int m2 = median - m1;
+
+            int l1 = (m1 == 0) ? INT_MIN : nums1[m1 - 1];
+            int l2 = (m2 == 0) ? INT_MIN : nums2[m2 - 1];
+            int r1 = (m1 == nums1.size()) ? INT_MAX : nums1[m1];
+            int r2 = (m2 == nums2.size()) ? INT_MAX : nums2[m2];
+
+            if (l1 <= r2 && l2 <= r1)
+                if ((nums1.size() + nums2.size()) % 2 == 0)
+                    return ((max(l1, l2) + min(r1, r2)) / 2.0);
+                else
+                    return max(l1, l2);
+            else if (l2 > r1)
+                low = m1 + 1;
             else
-                return max(l1, l2);
+                high = m1 - 1;
         }
-        else if (l2 > r1)
-            low = mid1 + 1;
-        else
-            high = mid1 - 1;
+        return 0.0;
     }
-    return 0.0;
-}
+};
 
 int main()
 {
-    int arr1[] = {1, 4, 7, 10, 12}, arr2[] = {2, 3, 6, 15};
-    cout << findMedianSortedArrays(arr1, 5, arr2, 4) << endl;
+    Solution s;
+    vector<int> nums1{1, 2}, nums2{3, 4};
+    cout << s.findMedianSortedArrays(nums1, nums2);
     return 0;
 }
